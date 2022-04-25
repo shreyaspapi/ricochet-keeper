cf61f67a520013ced37175d21711bc775b76d577
Shreyas Papinwar <spapinwar@gmail.com>
2022-04-25 19:35:53 +0100

    test

--- dags/ricochet_distribute_v2.py
+++ dags/ricochet_distribute_v2.py
@@ -103,6 +103,6 @@ for offset, (exchange_address, tokens) in enumerate(V2_EXCHANGE_ADDRESSES.items(
     current_nonce += 3
 
 
-    << distribute << update_a << update_b
+    distribute << update_a << update_b
 
 done
